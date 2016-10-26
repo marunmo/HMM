e0a02c9d029309f9fccea6a6f0c9eb93bcf59f62
Madhulekha <madhulekha.a@gmail.com>
2016-10-25 20:25:39 -0400

    HMM over random obervations using EM

---
+++ HMMHealth/hmm_rand_observations_analysis.py
@@ -0,0 +1,30 @@
+"""
+Gaussisan HMM of data generated randomly
+Uses Gaussian HMM on data generated randomly
+"""
+
+import numpy as np
+from hmmlearn.hmm import GaussianHMM
+
+
+# Generate random numbers for observations. Each observation has 2 attributes
+X=np.random.rand(100,2)
+
+# Run Gaussian HMM
+print "fitting to HMM and decoding ..."
+
+# Make an HMM instance and execute fit
+model = GaussianHMM(n_components=4, covariance_type="diag", n_iter=1000).fit(X)
+
+# Predict the optimal sequence of internal hidden state
+hidden_states = model.predict(X)
+
+# Print trained parameters
+print "Transition matrix"
+print model.transmat_
+
+print "Means and vars of each hidden state"
+for i in range(model.n_components):
+    print "{0}th hidden state".format(i)
+    print "mean = ", model.means_[i]
+    print "var = ", np.diag(model.covars_[i])
