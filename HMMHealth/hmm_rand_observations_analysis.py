990f7b717b537f46df76baa25af5a0e602472ce9
Madhulekha <madhulekha.a@gmail.com>
2016-11-21 13:56:47 -0500

    Sample data changes

--- HMMHealth/hmm_rand_observations_analysis.py
+++ HMMHealth/hmm_rand_observations_analysis.py
@@ -14,6 +14,7 @@ from numpy import genfromtxt
 
 
 def createSample(n,l,k):
+    #l=1
     #n is number of samples
     #l is number of sequences
     #k is number of components
@@ -25,8 +26,30 @@ def createSample(n,l,k):
 
     # The transition matrix, note that there are no transitions possible
     # between component 1 and 3
-    transmat_org = np.random.rand(k,k)
-    transmat_org = transmat_org / np.sum(transmat_org,1).reshape((k,1))
+    iden=np.identity(k)
+    probstate=np.random.rand(k)*0.4
+    pi = probstate * iden
+    #pi=np.identity(k) * 0.6
+    trans=np.zeros([k,k])
+
+    for i in range(0,k-1):
+        trans[i,i+1]=1.0
+
+    trans[k-1,0]=1.0
+
+    #print trans
+
+    transmat_temp = pi + trans
+    transmat_org = transmat_temp / np.sum(transmat_temp, 1).reshape((k, 1))
+
+    #print transmat_org
+
+    #print np.sum(transmat_org, 1).reshape((k, 1))
+
+    #/ (1 + probstate).reshape((k, 1))
+    #print transmat_org
+    #transmat_org = np.random.rand(k,k)
+    #transmat_org = transmat_org / np.sum(transmat_org,1).reshape((k,1))
     #print "transmat org",transmat_org
     #transmat_org = np.array([[0.7, 0.2, 0.0, 0.1],
                  #        [0.3, 0.5, 0.2, 0.0],
@@ -35,6 +58,12 @@ def createSample(n,l,k):
 
     # The means of each component
     means_org = np.random.rand(k,l)
+
+    for j in range(0,l):
+        for i in range(1,k):
+            means_org[i,j]=means_org[i-1,j]+100
+
+
         #np.array([[0.0,  0.0],
          #             [0.0, 11.0],
           #            [9.0, 10.0],
@@ -68,22 +97,27 @@ def training(trainX,trainZ,k,startprob_org,transmat_org,means_org,covars_org):
     #evaluating training
     hidden_states = model_p.predict(trainX)
     transmat=model_p.transmat_
+    #print transmat
     means=model_p.means_
     covars=model_p.covars_
     startprob=model_p.startprob_
 
     l = list(permutations(range(0, k)))
-    acctrain=1000
+    acctrain=-1
     min_covars=0
-    min_acc=1000
+    cur_acc=0
 
     for p in l:
+        #print p
+        #print "trainZ ",trainZ
         var_trainZ=trainZ
+        #print "var trainZ",var_trainZ
         for i in range(trainZ.shape[0]):
             var_trainZ[i]=p[trainZ[i]]
-        min_acc=np.mean( hidden_states == var_trainZ )
-        if(min_acc<acctrain):
-            acctrain=min_acc
+        cur_acc=np.mean( hidden_states == var_trainZ )
+        if(cur_acc>acctrain):
+            #print "max acc",cur_acc
+            acctrain=cur_acc
             perm_m=p
 
     # acctrain=norm(hidden_states-var_trainZ,2)
@@ -134,17 +168,20 @@ def testing(testX, testZ, k, model_p,perm_m):
 
 
 if __name__=="__main__":
-    print "Setting experiment values"
+    print("Setting experiment values")
 
-    input=genfromtxt('experiment_values.csv', delimiter=',',dtype='int')
+    input=genfromtxt('experiment_values_1.csv', delimiter=',',dtype='int')
     input=input[1:,1:]
-    outputarr = np.empty([51,12])
+    outputarr = np.empty([31,12])
 
 
-    for i in range(0,51):
+    for i in range(4,35):
         exp,n,k,l=input[i,:]
         trainX, trainZ,startprob_org,transmat_org,means_org,covars_org,testX, testZ=createSample(n,l,k)
         startTimeTrain = datetime.now()
+
+        #print np.sum(transmat_org, 1).reshape((k, 1))
+
         min_start, min_transmat, min_means, min_covars, acctrain, model_p, perm_m = training(trainX, trainZ, k,
                                                                                              startprob_org, transmat_org,
                                                                                              means_org, covars_org)
@@ -154,10 +191,14 @@ if __name__=="__main__":
 
         timeTest=datetime.now()-endTimeTrain
 
-        outputarr[i,:]= exp,timeTrain.total_seconds(),timeTest.total_seconds(),n,k,l,acctrain,acctest,min_start,min_transmat,min_means,min_covars
+        outputarr[i-4,:]= exp,timeTrain.total_seconds(),timeTest.total_seconds(),n,k,l,acctrain,acctest,min_start,min_transmat,min_means,min_covars
+
+        #print exp,timeTrain.total_seconds(),timeTest.total_seconds(),n,k,l,acctrain,acctest,min_start,min_transmat,min_means,min_covars
 
 
-    np.savetxt('experimentall.out',outputarr, delimiter=',')
+    #print outputarr
+
+    np.savetxt('experiment2_.out',outputarr, delimiter=',')
 
 
 
